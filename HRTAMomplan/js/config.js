c14a1074db29c4da92099f7c8683a6a177b00a84
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2022-09-07 14:37:23 +0200

    update datum en link

--- HRTAMomplan/js/config.js
+++ HRTAMomplan/js/config.js
@@ -4,12 +4,12 @@ var respecConfig =
   specStatus: "GN-DEF",
   specType: "HR",
   license: "cc-by-nd",
-  publishDate: "2022-09-01",
+  publishDate: "2022-09-07",
   authors: [
     {
       name: "Beheerder RO Standaarden",
       company: "Geonovum",
-	  companyURL: "https://www.geonovum.nl/"
+      companyURL: "https://www.geonovum.nl/"
     }
   ],
   editors: [
@@ -21,9 +21,9 @@ var respecConfig =
   ],
   shortName: "HRTAMomplan",
   format: "markdown",
-  github: "https://github.com/Geonovum/ro-HRTAMomplan/",
-  issueBase: "https://github.com/Geonovum/ro-HRTAMomplan/issues/",
-  edDraftURI: "https://Geonovum.github.io/ro-HRTAMomplan/",
-  publishISODate: "2022-09-01T00:00:00.000Z",
-  generatedSubtitle: "Vastgestelde versie 1 september 2022"
+  github: "https://github.com/Geonovum/ROST/",
+  issueBase: "https://github.com/Geonovum/ROST/issues/",
+  edDraftURI: "https://Geonovum.github.io/ROST/",
+  publishISODate: "2022-09-07T00:00:00.000Z",
+  generatedSubtitle: "Vastgestelde versie 7 september 2022"
 }
