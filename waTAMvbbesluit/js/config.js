4cf3ab913010c0a7b74b248a7268f5920b821d71
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2022-10-24 11:47:40 +0200

    update datum

--- waTAMvbbesluit/js/config.js
+++ waTAMvbbesluit/js/config.js
@@ -4,7 +4,7 @@ var respecConfig =
   specStatus: "GN-WV",
   specType: "WA",
   license: "cc-by-nd",
-  publishDate: "2022-09-07",
+  publishDate: "2022-10-24",
   authors: [
     {
       name: "beheerder RO Standaarden",
@@ -17,5 +17,5 @@ var respecConfig =
   github: "https://github.com/Geonovum/ROST/",
   issueBase: "https://github.com/Geonovum/ROST/issues/",
   edDraftURI: "https://Geonovum.github.io/ROST/",
-  generatedSubtitle: "Werkversie 7 september 2022"
+  generatedSubtitle: "Werkversie 24 oktober 2022"
 }
