720dff48e18045955f06929350d4ffcda96b10eb
Gijs Koedam <g.koedam@geonovum.nl>
2022-10-26 16:28:37 +0200

    PRPCP2008 als ReSpec bestand
    Omgezet in ReSpec in het kader van WCAG-proof maken van pdf-documenten (2021)

---
+++ PRPCP/PRPCP2008-v1.2.2/js/config.js
@@ -0,0 +1,28 @@
+var respecConfig =
+{
+  subtitle: "PRPCP2008",
+  pubDomain: "ro",
+  specStatus: "GN-DV",
+  specType: "PR",
+  license: "cc-by-nd",
+  publishDate: "2012-11-15",
+  authors: [
+    {
+      name: "Geonovum",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Geonovum",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  shortName: "PraktijkrichtlijnPlancontourEnPdf",
+  format: "markdown",
+  github: "https://github.com/Geonovum/ro-PraktijkrichtlijnPlancontourEnPdf",
+  issueBase: "https://github.com/Geonovum/ro-PraktijkrichtlijnPlancontourEnPdf/issues",
+  edDraftURI: "https://Geonovum.github.io/ro-PraktijkrichtlijnPlancontourEnPdf"
+}
---
+++ PRPCP/readme.md
@@ -0,0 +1,1 @@
+[Praktijkrichtlijn Plancontour en PDF (PRPCP2018)](https://geonovum.github.io/ROST/\PRPCP\PRPCP2008-v1.2.2/PRPCP2008-v1.2.2.html)  
