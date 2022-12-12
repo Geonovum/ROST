d050c51a8fc15829e8c86ba54d58fea22b7864e2
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2022-12-12 16:05:02 +0100

    update config.js 4xTAM

--- waTAMomver/js/config.js
+++ waTAMomver/js/config.js
@@ -1,21 +1,30 @@
 var respecConfig =
 {
   pubDomain: "ro",
-  specStatus: "GN-WV",
+  specStatus: "GN-DEF",
   specType: "WA",
   license: "cc-by-nd",
-  publishDate: "2022-12-07",
+ //publishDate: "2022-12-07",
   authors: [
     {
-      name: "beheerder RO Standaarden",
+      name: "Beheerder RO Standaarden",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+   editors: [
+    {
+      name: "Beheerteam RO Standaarden",
       company: "Geonovum",
       companyURL: "https://www.geonovum.nl/"
     }
   ],
   shortName: "waTAMomver",
   format: "markdown",
-  github: "https://github.com/Geonovum/ROST/",
-  issueBase: "https://github.com/Geonovum/ROST/issues/",
-  edDraftURI: "https://Geonovum.github.io/ROST/",
-  generatedSubtitle: "Werkversie 7 december 2022"
+  github: "https://github.com/Geonovum/ROST",
+  issueBase: "https://github.com/Geonovum/ROST/issues",
+  edDraftURI: "https://Geonovum.github.io/ROST",
+  generatedSubtitle: "Definitieve versie 7 december 2022"
+  previousPublishDate: "2022-11-14",  	    
+  previousMaturity: "GN-CV",  
 }
--- waTAMvbbesluit/js/config.js
+++ waTAMvbbesluit/js/config.js
@@ -1,23 +1,30 @@
 var respecConfig =
 {
   pubDomain: "ro",
-  specStatus: "GN-WV",
+  specStatus: "GN-DEF",
   specType: "WA",
   license: "cc-by-nd",
-  //publishDate: "2022-11-14",
+ //publishDate: "2022-12-07",
   authors: [
     {
-      name: "beheerder RO Standaarden",
+      name: "Beheerder RO Standaarden",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+   editors: [
+    {
+      name: "Beheerteam RO Standaarden",
       company: "Geonovum",
       companyURL: "https://www.geonovum.nl/"
     }
   ],
   shortName: "waTAMvbbesluit",
   format: "markdown",
-  github: "https://github.com/Geonovum/ROST/",
-  issueBase: "https://github.com/Geonovum/ROST/issues/",
-  edDraftURI: "https://Geonovum.github.io/ROST/",
- // generatedSubtitle: "Werkversie 25 oktober 2022",
+  github: "https://github.com/Geonovum/ROST",
+  issueBase: "https://github.com/Geonovum/ROST/issues",
+  edDraftURI: "https://Geonovum.github.io/ROST",
+  generatedSubtitle: "Definitieve versie 7 december 2022",
   previousPublishDate: "2022-11-14",  	    
-  previousMaturity: "GN-CV",                    
+  previousMaturity: "GN-CV",   
 }
