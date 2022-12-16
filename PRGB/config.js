c22b9c92ce1316f3196e4bf11407ba7bbfb52a3a
Jan van Gelder <43755492+JvGldr@users.noreply.github.com>
2022-12-16 17:24:50 +0100

    Aanpassingen tbv. Publicatie
    De config.js files bevatten een aantal omissies. Bij een multidoc repo moeten de verwijzingen niet naar de individuele documenten staan, maar naar de repo. Anders gaan de links naar 'doe mee' en issue log fout.

--- PRGB/config.js
+++ PRGB/config.js
@@ -11,6 +11,30 @@
 //-- Log . . . :  20191022 - GRK - PRGB2012 
 //-------------------------------------------------------------------------------------
 
+//-- Postprocessors -------------------------------------------------------------------
+
+//-- haalt gh-pages weg aan het eind van een URL
+//-- Stopt zodra de eerste is gevonden (want komt maar 1x voor)
+//-- JvG 2019-11-12
+function custGHPG(respecConfig)  
+{
+  var tags = document.getElementsByTagName("a");
+  var srch = "gh-pages";
+  var slen = srch.length;
+  var i;
+
+  for (i = 0; i < tags.length; i++) 
+  {
+    if(tags[i].href.indexOf(srch) > -1)
+    {
+      console.log(tags[i].href + " is gevonden");
+      tags[i].href = tags[i].href.substring(0, tags[i].href.length - slen);
+      console.log(tags[i].href + " is aangepast");
+      break;
+    }
+  } 
+}
+
 //-------------------------------------------------------------------------------------
 var respecConfig = 
 {
@@ -35,7 +59,7 @@ var respecConfig =
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   //-- de github repository waar je documentn in staat --------------------------------
-  github: "https://github.com/geonovum/ROST/PRGB",
+  github: "https://github.com/geonovum/ROST",
   editors: 
  [
   {
@@ -124,6 +148,7 @@ var respecConfig =
         company:    "Companynaam",
     },
   },
+  postProcess:[custGHPG],   //-- Optioneel voor een multi document repository
 
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
   previousPublishDate: "2018-10-23",  	    // Format is "YYY-MM-DD"
--- PRPCP/js/config.js
+++ PRPCP/js/config.js
@@ -1,3 +1,27 @@
+//-- Postprocessors -------------------------------------------------------------------
+
+//-- haalt gh-pages weg aan het eind van een URL
+//-- Stopt zodra de eerste is gevonden (want komt maar 1x voor)
+//-- JvG 2019-11-12
+function custGHPG(respecConfig)  
+{
+  var tags = document.getElementsByTagName("a");
+  var srch = "gh-pages";
+  var slen = srch.length;
+  var i;
+
+  for (i = 0; i < tags.length; i++) 
+  {
+    if(tags[i].href.indexOf(srch) > -1)
+    {
+      console.log(tags[i].href + " is gevonden");
+      tags[i].href = tags[i].href.substring(0, tags[i].href.length - slen);
+      console.log(tags[i].href + " is aangepast");
+      break;
+    }
+  } 
+}
+
 var respecConfig =
 {
 //  subtitle: "PRPCP2008",
@@ -22,11 +46,12 @@ var respecConfig =
       companyURL: "https://www.geonovum.nl/"
     }
   ],
+  postProcess:[custGHPG],   //-- Optioneel voor een multi document repository
   shortName: "PRPCP2008",
   format: "markdown",
-  github: "https://github.com/Geonovum/ro-PRPCP2008/",
-  issueBase: "https://github.com/Geonovum/ro-PRPCP2008/issues/",
-  edDraftURI: "https://Geonovum.github.io/ro-PRPCP2008/",
+  github: "https://github.com/Geonovum/ROST/",
+  //issueBase: "https://github.com/Geonovum/ROST/issues/",
+  //edDraftURI: "https://Geonovum.github.io/ro-PRPCP2008/",
   publishISODate: "2022-12-13T00:00:00.000Z",
   generatedSubtitle: "Vastgestelde versie 13 december 2022"
 }
