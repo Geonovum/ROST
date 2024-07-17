a14404e537654d089caf396687f7a1af8c26aa13
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2024-07-17 15:02:15 +0200

    PRAMVB update config bestanden

---
+++ PRAMvB/js/document-config.js
@@ -0,0 +1,26 @@
+var documentConfig =
+{
+  title: "PRAMvB",
+  shortName: "PRAMvB",
+  pubDomain: "ow",
+  specStatus: "wv",
+  specType: "al",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ow/PRAMvB/"
+  ],
+  publishDate: "2024-07-16",
+  publishVersion: "0.0.1",
+  thisVersion: [
+    "https://geonovum.github.io/ow-PRAMvB/"
+  ],
+  edDraftURI: "https://geonovum.github.io/ow-PRAMvB/",
+  canonicalURI: "https://docs.geostandaarden.nl/ow/PRAMvB/",
+  editors: [
+    {
+      name: "Gerard Wolbers",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ]
+}
