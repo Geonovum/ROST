d61a6189292778f30eb61649ca16428b2be40e45
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-08-14 16:39:25 +0200

    Omgezet naar Logius

---
+++ HRDoO/js/document-config.js
@@ -0,0 +1,41 @@
+var documentConfig =
+{
+  title: "Handreiking Data op Orde (DoO)",
+  shortName: "doo",
+  pubDomain: "ro",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/doo/"
+  ],
+  publishDate: "2024-08-14",
+  publishVersion: "2.0.2",
+  thisVersion: [
+    "https://geonovum.github.io/ro-doo/"
+  ],
+  previousPublishDate: "2024-04-04",
+  previousPublishVersion: "2.0.1",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/ro/def-hr-doo-20240404/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/ro-doo/",
+  issueBase: "https://github.com/geonovum/ro-doo/issues/",
+  edDraftURI: "https://geonovum.github.io/ro-doo/",
+  canonicalURI: "https://docs.geostandaarden.nl/ro/doo/",
+  authors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/ro-standaarden-ruimtelijke-ordening/"
+    }
+  ]
+}
