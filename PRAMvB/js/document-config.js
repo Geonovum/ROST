080177659b2d769d0ad158606b5167865ee3befa
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-08-14 22:03:14 +0200

    Omgezet naar Logius

---
+++ PRAMvB/js/document-config.js
@@ -0,0 +1,41 @@
+var documentConfig =
+{
+  title: "Prakijkrichtlijn Algemene Maatregel van Bestuur 2012 (PRAMvB)",
+  shortName: "amvb2012",
+  pubDomain: "ro",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/amvb2012/"
+  ],
+  publishDate: "2024-08-14",
+  publishVersion: "1.1.7",
+  thisVersion: [
+    "https://geonovum.github.io/ro-amvb2012/"
+  ],
+  previousPublishDate: "2022-12-15",
+  previousPublishVersion: "1.1.6",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/ro/def-hr-amvb2012-20221215/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/ro-amvb2012/",
+  issueBase: "https://github.com/geonovum/ro-amvb2012/issues/",
+  edDraftURI: "https://geonovum.github.io/ro-amvb2012/",
+  canonicalURI: "https://docs.geostandaarden.nl/ro/amvb2012/",
+  authors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/ro-standaarden-ruimtelijke-ordening/"
+    }
+  ]
+}
