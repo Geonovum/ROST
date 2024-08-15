0811dbb866fef86470beb34156fb098e7d2758b3
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-08-15 10:49:38 +0200

    Omgezet naar Logius

---
+++ PRTRI/js/document-config.js
@@ -0,0 +1,41 @@
+var documentConfig =
+{
+  title: "Praktijkrichtlijn Toegankelijkheid Ruimtelijke Instrumenten 2012 (PTRI2012)",
+  shortName: "tri2012",
+  pubDomain: "ro",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/tri2012/"
+  ],
+  publishDate: "2024-08-15",
+  publishVersion: "1.5.1",
+  thisVersion: [
+    "https://geonovum.github.io/ro-tri2012/"
+  ],
+  previousPublishDate: "2023-12-20",
+  previousPublishVersion: "1.5.0",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/ro/def-hr-tri2012-20231220/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/ro-tri2012/",
+  issueBase: "https://github.com/geonovum/ro-tri2012/issues/",
+  edDraftURI: "https://geonovum.github.io/ro-tri2012/",
+  canonicalURI: "https://docs.geostandaarden.nl/ro/tri2012/",
+  authors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/ro-standaarden-ruimtelijke-ordening/"
+    }
+  ]
+}
