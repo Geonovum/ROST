5cb8427b511958127115ee73534d07fd2e8b4c7e
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-08-15 10:16:00 +0200

    Omgezet naar Logius

---
+++ PRPT/js/document-config.js
@@ -0,0 +1,41 @@
+var documentConfig =
+{
+  title: "Praktijkrichtlijn voor Planteksten 2012 (PRPT2012)",
+  shortName: "pt2012",
+  pubDomain: "ro",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/pt2012/"
+  ],
+  publishDate: "2024-08-15",
+  publishVersion: "1.2.3",
+  thisVersion: [
+    "https://geonovum.github.io/ro-pt2012/"
+  ],
+  previousPublishDate: "2022-12-15",
+  previousPublishVersion: "1.2.2",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/ro/def-hr-pt2012-20221215/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/ro-pt2012/",
+  issueBase: "https://github.com/geonovum/ro-pt2012/issues/",
+  edDraftURI: "https://geonovum.github.io/ro-pt2012/",
+  canonicalURI: "https://docs.geostandaarden.nl/ro/pt2012/",
+  authors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/ro-standaarden-ruimtelijke-ordening/"
+    }
+  ]
+}
