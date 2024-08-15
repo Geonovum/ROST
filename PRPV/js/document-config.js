0b10c52b5112f494371be3fd8d8bad035713faa9
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-08-15 10:26:40 +0200

    Omgezet naar Logius

---
+++ PRPV/js/document-config.js
@@ -0,0 +1,41 @@
+var documentConfig =
+{
+  title: "Praktijkrichtlijn Provinciale Verordening 2012 (PRPV2012)",
+  shortName: "pv2012",
+  pubDomain: "ro",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/pv2012/"
+  ],
+  publishDate: "2024-08-15",
+  publishVersion: "1.1.7",
+  thisVersion: [
+    "https://geonovum.github.io/ro-pv2012/"
+  ],
+  previousPublishDate: "2022-12-15",
+  previousPublishVersion: "1.1.6",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/ro/def-hr-pv2012-20221215/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/ro-pv2012/",
+  issueBase: "https://github.com/geonovum/ro-pv2012/issues/",
+  edDraftURI: "https://geonovum.github.io/ro-pv2012/",
+  canonicalURI: "https://docs.geostandaarden.nl/ro/pv2012/",
+  authors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/ro-standaarden-ruimtelijke-ordening/"
+    }
+  ]
+}
