fb679a2088c618142b0bb25237033745a9956df2
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-08-15 10:06:29 +0200

    Omgezet naar Logius

---
+++ PRGB/js/document-config.js
@@ -0,0 +1,41 @@
+var documentConfig =
+{
+  title: "Praktijkrichtlijn Gebiedsgerichte Besluiten 2012 (PRGB2012)",
+  shortName: "gb2012",
+  pubDomain: "ro",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/gb2012/"
+  ],
+  publishDate: "2024-08-15",
+  publishVersion: "1.3.7",
+  thisVersion: [
+    "https://geonovum.github.io/ro-gb2012/"
+  ],
+  previousPublishDate: "2022-12-15",
+  previousPublishVersion: "1.3.6",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/ro/def-hr-gb2012-20221215/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/ro-gb2012/",
+  issueBase: "https://github.com/geonovum/ro-gb2012/issues/",
+  edDraftURI: "https://geonovum.github.io/ro-gb2012/",
+  canonicalURI: "https://docs.geostandaarden.nl/ro/gb2012/",
+  authors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/ro-standaarden-ruimtelijke-ordening/"
+    }
+  ]
+}
