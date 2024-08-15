e9762bb8197f340349a6bf3ffbc39d901d2f901d
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-08-15 09:39:24 +0200

    Omgezet naar Logius

---
+++ PRBP/js/document-config.js
@@ -0,0 +1,41 @@
+var documentConfig =
+{
+  title: "Praktijkrichtlijn Bestemmingsplannen 2012 (PRBP2012)",
+  shortName: "bp2012",
+  pubDomain: "ro",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/bp2012/"
+  ],
+  publishDate: "2024-08-15",
+  publishVersion: "1.3.7",
+  thisVersion: [
+    "https://geonovum.github.io/ro-bp2012/"
+  ],
+  previousPublishDate: "2022-12-15",
+  previousPublishVersion: "1.3.6",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/ro/def-hr-bp2012-20221215/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/ro-bp2012/",
+  issueBase: "https://github.com/geonovum/ro-bp2012/issues/",
+  edDraftURI: "https://geonovum.github.io/ro-bp2012/",
+  canonicalURI: "https://docs.geostandaarden.nl/ro/bp2012/",
+  authors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/ro-standaarden-ruimtelijke-ordening/"
+    }
+  ]
+}
