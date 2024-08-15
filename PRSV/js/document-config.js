fdf0254036c9829ba17614dbad1ca996e6735089
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-08-15 10:41:39 +0200

    Omgezet naar Logius

---
+++ PRSV/js/document-config.js
@@ -0,0 +1,41 @@
+var documentConfig =
+{
+  title: "Praktijkrichtlijn Structuurvisies 2012 (PRSV2012)",
+  shortName: "sv2012",
+  pubDomain: "ro",
+  specStatus: "wv",
+  specType: "hr",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/ro/sv2012/"
+  ],
+  publishDate: "2024-08-15",
+  publishVersion: "1.1.6",
+  thisVersion: [
+    "https://geonovum.github.io/ro-sv2012/"
+  ],
+  previousPublishDate: "2022-12-15",
+  previousPublishVersion: "1.1.5",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/ro/def-hr-sv2012-20221215/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/ro-sv2012/",
+  issueBase: "https://github.com/geonovum/ro-sv2012/issues/",
+  edDraftURI: "https://geonovum.github.io/ro-sv2012/",
+  canonicalURI: "https://docs.geostandaarden.nl/ro/sv2012/",
+  authors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Beheerteam DRO",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/geo-standaarden/ro-standaarden-ruimtelijke-ordening/"
+    }
+  ]
+}
