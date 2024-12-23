f5675482060b5917937bfaa02c915af4b23ef2bc
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2024-12-23 13:54:53 +0100

    update ROST PRAMVB config.js

--- PRAMvB/js/document-config.js
+++ PRAMvB/js/document-config.js
@@ -3,21 +3,18 @@ var documentConfig =
   title: "Prakijkrichtlijn Algemene Maatregel van Bestuur 2012 (PRAMvB)",
   shortName: "amvb2012",
   pubDomain: "ro",
-  specStatus: "def",
+  specStatus: "wv",
   specType: "pr",
   license: "cc-by-nd",
   latestVersion: [
     "https://docs.geostandaarden.nl/ro/amvb2012/"
   ],
-  publishDate: "2024-12-20",
-  publishVersion: "1.1.8",
-  thisVersion: [
-    "https://docs.geostandaarden.nl/ro/def-pr-amvb2012-20241220/"
-  ],
-  previousPublishDate: "2024-02-14",
-  previousPublishVersion: "1.1.7",
+  publishDate: "",
+  publishVersion: "1.1.9",
+  previousPublishDate: "2024-12-20",
+  previousPublishVersion: "1.1.8",
   prevVersion: [
-    "https://docs.geostandaarden.nl/ro/def-pr-amvb2012-20240214/"
+    "https://docs.geostandaarden.nl/ro/def-pr-amvb2012-20241220/"
   ],
   previousMaturity: "def",
   github: "https://github.com/geonovum/ROST/",
