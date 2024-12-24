4404ce11e07d3e5eea8fbcb575c47fddd622fd92
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2024-12-24 12:47:23 +0100

    update PRPV config.js -> werkversie

--- PRPV/js/document-config.js
+++ PRPV/js/document-config.js
@@ -3,19 +3,19 @@ var documentConfig =
   title: "Praktijkrichtlijn Provinciale Verordening 2012 (PRPV2012)",
   shortName: "pv2012",
   pubDomain: "ro",
-  specStatus: "def",
+  specStatus: "wv",
   specType: "pr",
   license: "cc-by-nd",
   latestVersion: [
     "https://docs.geostandaarden.nl/ro/pv2012/"
   ],
-  publishDate: "2024-12-24",
-  publishVersion: "1.1.8",
+  publishDate: "",
+  publishVersion: "1.1.9",
   thisVersion: [
     "https://docs.geostandaarden.nl/ro/def-pr-pv2012-20241224/"
   ],
-  previousPublishDate: "2024-02-14",
-  previousPublishVersion: "1.1.7",
+  previousPublishDate: "2024-12-24",
+  previousPublishVersion: "1.1.8",
   prevVersion: [
     "https://docs.geostandaarden.nl/ro/def-pr-pv2012-20240214/"
   ],
