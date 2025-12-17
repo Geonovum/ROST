28ce45612db5b14892d70b62ea350abf5bdbe13e
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2025-12-17 11:08:43 +0100

    update PRTRI

--- PRTRI/js/document-config.js
+++ PRTRI/js/document-config.js
@@ -3,18 +3,18 @@ var documentConfig =
   title: "Praktijkrichtlijn Toegankelijkheid Ruimtelijke Instrumenten 2012 (PTRI2012)",
   shortName: "tri2012",
   pubDomain: "ro",
-  specStatus: "wv",
+  specStatus: "def",
   specType: "pr",
   license: "cc-by-nd",
   latestVersion: [
     "https://docs.geostandaarden.nl/ro/tri2012/"
   ],
-  //publishDate: "2025-05-16",
-  //publishVersion: "1.7",
-  //thisVersion: [
-  //  "https://docs.geostandaarden.nl/ro/def-pr-tri2012-20250516/"
- // ],
-  previousPublishDate: "2025-05-1",
+  publishDate: "2025-12-17",
+  publishVersion: "1.8",
+  thisVersion: [
+  "https://docs.geostandaarden.nl/ro/def-pr-tri2012-20251217/"
+  ],
+  previousPublishDate: "2025-05-16",
   previousPublishVersion: "1.7",
   prevVersion: [
     "https://docs.geostandaarden.nl/ro/def-pr-tri2012-20250516/"
